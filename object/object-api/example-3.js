b92fc7a440ad35dcd20473e1416d893acd0494aa
Ederson Macedo <ederson.mos@gmail.com>
2023-01-17 15:42:24 -0300

    Example created compare objects

--- object/object-api/example-3.js
+++ object/object-api/example-3.js
@@ -1,3 +1,25 @@
+function isEqual(object1, object2) {
+    let equal = true;
+
+    if (object1.length !== object2.length) {
+        return false;
+    }
+    
+    if (typeof object1 !== 'object' || typeof object2 !== 'object') {
+        return false;
+    }
+
+
+    for (let key in object1) {
+        if (object1[key] !== object1[key]) {
+            equal = false;
+            break;
+        }
+    }
+
+    return equal;
+}
+
 const javascript = {
     name: "JavaScript",
     year: 1995,
@@ -12,8 +34,11 @@ console.log(javascript);
 console.log(javascript1);
 // expect output: { name: 'JavaScript', year: 1995, paradigm: 'OO and Functional' }
 
-console.log(Object.is(javascript, javascript));
-// expect output: true
+console.log('Compare javascript, javascript', Object.is(javascript, javascript));
+// expect output: 'Compare javascript, javascript Object.is' true
 
-console.log(Object.is(javascript1, javascript));
-// expect output: false
+console.log('Compare javascript1, javascript Object.is', Object.is(javascript1, javascript));
+// expect output: 'Compare javascript1, javascript Object.is' false
+
+console.log('Compare javascript1, javascript method isEqual', isEqual(javascript1, javascript));
+// expect output: 'Compare javascript1, javascript method isEqual' true
