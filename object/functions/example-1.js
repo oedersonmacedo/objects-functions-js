669bc4074fd38ab08e2d88c73f2024738daedd9b
Ederson Macedo <ederson.mos@gmail.com>
2023-01-19 06:58:34 -0300

    Created example of a function expression and declaration

---
+++ object/functions/example-1.js
@@ -0,0 +1,12 @@
+function sumDeclaration(a, b) {
+    return a + b;
+}
+console.log(sumDeclaration(2, 2));
+// expect output: 4
+
+const sumExpression = function(a, b) {
+    return a + b;
+}
+console.log(sumExpression(2, 2));
+// expect output: 4
+
