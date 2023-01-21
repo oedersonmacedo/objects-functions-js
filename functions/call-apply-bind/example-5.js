1c77b08d11f8edf198aa0d1451895fe9eb815ab1
Ederson Macedo <ederson.mos@gmail.com>
2023-01-21 09:01:30 -0300

    Example created with function call, to pass parameter

---
+++ functions/call-apply-bind/example-5.js
@@ -0,0 +1,9 @@
+const calculateArea = function(fn) {
+    return fn(Math.PI * Math.pow(this.radius, 2));
+};
+const circle = {
+    radius: 10,
+    calculateArea
+};
+console.log(calculateArea.call(circle, Math.round));
+// expect output: 314
