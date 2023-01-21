9981d3508a57866e26c4035b955804037505405d
Ederson Macedo <ederson.mos@gmail.com>
2023-01-21 15:58:53 -0300

    Example about arrow function to remove parentheses

---
+++ functions/arrow-function/example-4.js
@@ -0,0 +1,10 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const calculator = fn => (a, b) => fn(a, b);
+// With only one parameter, it's possible to remove the parentheses
+
+console.log(calculator(sum)(2, 2));
+// expect output: 4
+
+console.log(calculator(subtract)(2, 2));
+// expect output: 0
