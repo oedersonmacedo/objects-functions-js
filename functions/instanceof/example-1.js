eba09c17bc452fe2f6b8d0bf21f1b19899b1fee4
Ederson Macedo <ederson.mos@gmail.com>
2023-01-21 10:49:04 -0300

    Example created using instanceof in a simple way

---
+++ functions/instanceof/example-1.js
@@ -0,0 +1,9 @@
+const date = new Date();
+console.log(date instanceof Date);
+// exepect output: true
+
+console.log(date instanceof Object);
+// exepect output: true
+
+console.log(date instanceof Array);
+// exepect output: false
