d457718882739f6e3678761e90a49d8476186d3c
Ederson Macedo <ederson.mos@gmail.com>
2023-01-21 08:46:58 -0300

    Refactore value call

--- functions/call-apply-bind/example-2.js
+++ functions/call-apply-bind/example-2.js
@@ -2,9 +2,9 @@ const calculateArea = function() {
     return Math.PI * Math.pow(this.radius, 2);
 };
 const circle = {
-    radius: 10,
+    radius: 5,
     calculateArea
 };
 
 console.log(calculateArea.call(circle));
-// expect output: 314.1592653589793
+// expect output: 78.53981633974483
