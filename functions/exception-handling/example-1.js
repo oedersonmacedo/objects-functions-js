fba1217b6aaad75f9e2433782ec539a41f5c0f2f
Ederson Macedo <ederson.mos@gmail.com>
2023-01-23 07:31:37 -0300

    Example generate error with exception handling

---
+++ functions/exception-handling/example-1.js
@@ -0,0 +1,14 @@
+const Rectangle = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.calculateArea = function() {
+        if (this.x > 0 && this.y > 0) {
+            return this.x * this.y;
+        } else {
+            throw "Invalid value for x or y";
+        }
+    }
+};
+const rectangle = new Rectangle(-10, -2);
+console.log(rectangle.calculateArea());
+// GENERATE ERROR: 'Invalid value for x or y'
